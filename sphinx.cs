2114b5c88c954a0507059b8180da1cd4a83773a5
Student <example@example.com>
2020-02-24 14:18:01 -0800

    add randomizer

--- sphinx.cs
+++ sphinx.cs
@@ -1,16 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Riddles
 {
   static void Main()
   {
+    Random rand = new Random();
     Dictionary<string, string> riddles = new Dictionary<string, string>(){
       {"footsteps", "The more you take, the more you leave behind. What am I?"},
       {"fence", "What runs around the whole yard without moving?"},
       {"telephone", "You answer me, although I never ask you questions."}
       };
-   foreach(KeyValuePair<string, string> entry in riddles)
+    Dictionary<string, string> randRiddle = riddles.OrderBy(x=>rand.Next()).ToDictionary(item => item.Key, item => item.Value);
+   foreach(KeyValuePair<string, string> entry in randRiddle)
     {
         Console.WriteLine(entry.Value);
         string answer = Console.ReadLine();
