586fc3ff47e5b0b73048fbc96a28419be527e437
Student <example@example.com>
2020-02-24 10:30:43 -0800

    iterate through a dictionary of riddles and show the next if you get it right break if you get it wrong

--- sphinx.cs
+++ sphinx.cs
@@ -5,24 +5,24 @@ class Riddles
 {
   static void Main()
   {
-    // Dictionary<string, string> riddles = new Dictionary<string, string>(){
-    //   {"footsteps", "The more you take, the more you leave behind. What am I?"},
-    //   {"fence", "What runs around the whole yard without moving?"},
-    //   {"telephone", "You answer me, although I never ask you questions."}
-    //   }
-    // foreach(KeyValuePair<string, string> entry in riddles)
-    //   {
-    //       string thisRiddle = riddles[entry];
-    //   }
-    Console.WriteLine("The more you take, the more you leave behind. What am I?");
-    string answer = Console.ReadLine();
-    if(answer.ToLower()=="footsteps")
+    Dictionary<string, string> riddles = new Dictionary<string, string>(){
+      {"footsteps", "The more you take, the more you leave behind. What am I?"},
+      {"fence", "What runs around the whole yard without moving?"},
+      {"telephone", "You answer me, although I never ask you questions."}
+      };
+   foreach(KeyValuePair<string, string> entry in riddles)
     {
-      Console.WriteLine("Correct!");
-    }
-    else
-     {
-      Console.WriteLine("Wrong! you got ate.....");
+        Console.WriteLine(entry.Value);
+        string answer = Console.ReadLine();
+        if(answer.ToLower()==entry.Key)
+        {
+          Console.WriteLine("Correct! Next Riddle:");
+        }
+        else
+        {
+          Console.WriteLine("Wrong! you got ate.....");
+          break;
+        }
     }
   }
 }
